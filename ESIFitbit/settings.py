8ddd4d5b5d9da93af3bd45f4ee204897470e7661
Jeffrey Kirchner <jkirchner@gmail.com>
2020-12-10 11:57:17 -0800

    require secure cookies

--- ESIFitbit/settings.py
+++ ESIFitbit/settings.py
@@ -104,14 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
+
+#cookies
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 #cron jobs
 CRON_CLASSES = [
