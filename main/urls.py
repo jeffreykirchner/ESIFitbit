eed4386feefeb9d3210590942705b790a400a900
Jeffrey Kirchner <jkirchner@gmail.com>
2021-01-20 10:06:51 -0800

    add test page

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import include,url
 from django.conf import settings
 from . import views
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
 
 urlpatterns = [
     #admin site
@@ -13,12 +14,7 @@ urlpatterns = [
     #account control
     path('',views.Home,name='home'),                 #direct user by subject type
     path('accounts/', include('django.contrib.auth.urls')),  #django built in account registrations
-    # path('profile/', views.updateProfile,name='profile'),    #custom profile 
-    # path('accounts/profile/', views.updateProfile,name='profile'), #custom profile         
-    # path('profileCreate/',views.profileCreate,name='profileCreate'),
-    # path('profileVerify/<token>/',views.profileVerify,name='profileVerify'),
-    # path('profileVerifyResend/',views.profileVerifyResend,name='profileVerifyResend'),
-    
+       
     #staff
     path('staffHome/',views.Staff_Home,name='staff_home'),
     path('session/<id>/',views.Staff_Session,name='staff_session'),
@@ -26,8 +22,11 @@ urlpatterns = [
     #subject
     path('subjectHome/',views.Home,name='home2'),
     path('subjectHome/<uuid:id>/',csrf_exempt(views.Subject_Home),name='subject_home'),
-   
 
+    #test page
+    #path('test/',TemplateView.as_view(template_name="test.html"),name='test'),
+   
+    #fitbit
     path('fitBit/',views.fitBit,name='fitBit'),
     
 ]
