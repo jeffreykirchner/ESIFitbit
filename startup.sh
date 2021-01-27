ca02a5c720f65665c21d1d98763521f3fb36a00e
Jeffrey Kirchner <jkirchner@gmail.com>
2021-01-27 12:01:37 -0800

    update time out

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 python manage.py migrate
-gunicorn --bind=0.0.0.0 --timeout 30 --max-requests 500 --max-requests-jitter 10  ESIFitbit.wsgi
+gunicorn --bind=0.0.0.0 --timeout 60 --max-requests 500 --max-requests-jitter 10  ESIFitbit.wsgi
