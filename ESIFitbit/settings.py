803066c4010c9c3bca7bc12c62fe46b6f59b3d32
Jeffrey Kirchner <jkirchner@gmail.com>
2021-01-27 16:24:52 -0800

    remove extra "pass"

--- ESIFitbit/settings.py
+++ ESIFitbit/settings.py
@@ -24,8 +24,6 @@ except ImportError:
     except ImportError:
         pass
 
-    pass
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
