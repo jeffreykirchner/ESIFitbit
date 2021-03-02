54eddcc6406b528e401a154c53818c75d3519ced
Jeffrey Kirchner <jkirchner@gmail.com>
2021-03-02 10:27:36 -0800

    add gender questions

--- main/forms/session_subject_questionnaire2_form.py
+++ main/forms/session_subject_questionnaire2_form.py
@@ -3,43 +3,62 @@ from main.models import Session_subject_questionnaire2
 from django.contrib.auth.models import User
 from django.forms import ModelChoiceField
 import pytz
-from main.globals.likertScales import Yes_No,Likert_change
+from main.globals.likertScales import Yes_No, Likert_change, SexAtBirth, GenderIdentity
 from django.utils.safestring import mark_safe
 
 class Session_subject_questionnaire2_form(forms.ModelForm):
         
     sleep_changed = forms.TypedChoiceField(label='As a result of participating in this study, I slept _________________ while I was participating, then before the study.', 
-                                         choices=Likert_change.choices,    
-                                         initial=Likert_change.DEFAULT,         
-                                         widget=forms.Select(attrs={}))
+                                           choices=Likert_change.choices,    
+                                           initial=Likert_change.DEFAULT,         
+                                           widget=forms.Select(attrs={}))
     
     sleep_changed_explaination = forms.CharField(label='How else did your sleep habits change? (Optional)',
-                                        required=False,
-                                        widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
+                                                 required=False,
+                                                 widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
 
     exercise_changed = forms.TypedChoiceField(label='As a result of participating in this study, I exercised _________________ while I was participating, then before the study.', 
-                                         choices=Likert_change.choices,    
-                                         initial=Likert_change.DEFAULT,         
-                                         widget=forms.Select(attrs={}))
+                                              choices=Likert_change.choices,    
+                                              initial=Likert_change.DEFAULT,         
+                                              widget=forms.Select(attrs={}))
     
     exercise_changed_explaination = forms.CharField(label='How else did your exercise habits change? (Optional)',
-                                        required=False,
-                                        widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
+                                                    required=False,
+                                                    widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
 
     health_concern = forms.TypedChoiceField(label='As a result of participating in this study, I will act with ____________________ concern for my health in the future. ', 
-                                         choices=Likert_change.choices,    
-                                         initial=Likert_change.DEFAULT,         
-                                         widget=forms.Select(attrs={}))
+                                            choices=Likert_change.choices,    
+                                            initial=Likert_change.DEFAULT,         
+                                            widget=forms.Select(attrs={}))
     
     health_concern_explaination = forms.CharField(label=mark_safe('If you anticipate a change: how will you change?<br>If do NOT anticipate a change: why won’t you change?'),
-                                        required=True,
-                                        widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
+                                                  required=True,
+                                                  widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
     
     holiday_break_explaination = forms.CharField(label=mark_safe('How did you spend your spring break?'),
-                                        required=True,
-                                        widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
+                                                 required=True,
+                                                 widget=forms.Textarea(attrs={"rows":"5", "cols":"75"}))
+
+    sex_at_birth = forms.TypedChoiceField(label='What was your sex at birth?', 
+                                          required=True,
+                                          choices=SexAtBirth.choices,    
+                                          initial=SexAtBirth.DEFAULT,         
+                                          widget=forms.Select(attrs={}))
+    
+    gender_identity = forms.TypedChoiceField(label='To which gender identity do you most identify?', 
+                                             required=True,  
+                                             choices=GenderIdentity.choices,    
+                                             initial=GenderIdentity.DEFAULT,         
+                                             widget=forms.Select(attrs={}))
+
+    gender_identity_fillin = forms.CharField(label="",
+                                             required=False,
+                                             widget=forms.Textarea(attrs={}))
+
 
 
     class Meta:
         model=Session_subject_questionnaire2
-        exclude=['session_subject']
+        fields=('sleep_changed', 'sleep_changed_explaination', 'exercise_changed', 'exercise_changed_explaination', 
+                'health_concern', 'health_concern_explaination', 'holiday_break_explaination' , 'sex_at_birth', 'gender_identity',
+                'gender_identity_fillin')
--- main/models/session_subject_questionnaire2.py
+++ main/models/session_subject_questionnaire2.py
@@ -7,23 +7,27 @@ import random
 from django.utils.crypto import get_random_string
 
 from main.models import Session_subject
-from main.globals.likertScales import Likert_change
+from main.globals.likertScales import Likert_change, GenderIdentity, SexAtBirth
 
 #gloabal parameters for site
 class Session_subject_questionnaire2(models.Model):
 
-    session_subject = models.ForeignKey(Session_subject,on_delete=models.CASCADE,related_name="Session_subject_questionnaire2")
+    session_subject = models.ForeignKey(Session_subject, on_delete=models.CASCADE, related_name="Session_subject_questionnaire2")
 
-    sleep_changed = models.CharField(max_length=100, choices=Likert_change.choices,verbose_name = 'Sleep Change Post')
-    sleep_changed_explaination = models.CharField(max_length = 10000, default = '',verbose_name = 'Sleep Change Post Explanation')
+    sleep_changed = models.CharField(max_length=100, choices=Likert_change.choices, verbose_name='Sleep Change Post')
+    sleep_changed_explaination = models.CharField(max_length=10000, default='', verbose_name='Sleep Change Post Explanation')
     
-    exercise_changed = models.CharField(max_length=100, choices=Likert_change.choices,verbose_name = 'Exercise Change Post')
-    exercise_changed_explaination = models.CharField(max_length = 10000, default = '',verbose_name = 'Exercise Changed Post Explanation')
+    exercise_changed = models.CharField(max_length=100, choices=Likert_change.choices, verbose_name='Exercise Change Post')
+    exercise_changed_explaination = models.CharField(max_length=10000, default='', verbose_name='Exercise Changed Post Explanation')
 
-    health_concern = models.CharField(max_length=100, choices=Likert_change.choices,verbose_name = 'Health Concern Post')
-    health_concern_explaination = models.CharField(max_length = 10000, default = '',verbose_name = 'Health Concern Post Explanation')
+    health_concern = models.CharField(max_length=100, choices=Likert_change.choices, verbose_name='Health Concern Post')
+    health_concern_explaination = models.CharField(max_length=10000, default='', verbose_name='Health Concern Post Explanation')
 
-    holiday_break_explaination = models.CharField(max_length = 10000, default = '',verbose_name = 'How did you spend your last break?')
+    holiday_break_explaination = models.CharField(max_length=10000, default='',verbose_name='How did you spend your last break?')
+
+    sex_at_birth = models.CharField(max_length=100, choices=SexAtBirth.choices, verbose_name='What was your sex at birth?')
+    gender_identity = models.CharField(max_length=100, choices=GenderIdentity.choices, verbose_name='To which gender identity do you most identify?')
+    gender_identity_fillin = models.CharField(max_length=1000, default='', verbose_name='To which gender identity do you most identify (Fill in)?')
 
     def __str__(self):
         return "Post Questionnaire"
@@ -44,6 +48,10 @@ class Session_subject_questionnaire2(models.Model):
         self.health_concern_explaination = get_random_string(length=100)
 
         self.holiday_break_explaination = get_random_string(length=100)
+
+        self.sex_at_birth = "Male"
+        self.gender_identity = "Man"
+        self.gender_identity_fillin = get_random_string(length=100)
 
         self.save()
 
