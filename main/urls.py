7d7c8b60b2dcfb0cc05f98efea23e09794c1486d
Jeffrey Kirchner <jkirchner@gmail.com>
2021-03-15 19:01:18 -0700

    doc string

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
+'''
+main url patterns
+'''
 from django.contrib import admin
 from django.urls import path,re_path
 from django.views.generic.base import RedirectView
-from django.conf.urls import include,url
+from django.conf.urls import include, url
 from django.conf import settings
 from . import views
 from django.views.decorators.csrf import csrf_exempt
