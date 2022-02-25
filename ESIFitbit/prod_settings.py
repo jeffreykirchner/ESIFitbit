c44da028404acdc4ec4eccf9b3acc874739911f7
Jeffrey Kirchner <kirchner@chapman.edu>
2022-02-25 15:13:25 -0800

    Consent form to rich text. Fixes #251.

--- ESIFitbit/prod_settings.py
+++ ESIFitbit/prod_settings.py
@@ -79,7 +79,7 @@ LOGGING = {
 #tiny MCE
 TINYMCE_DEFAULT_CONFIG = {
     "height" : 600,
-    "menubar": False,
+    "menubar": True,
     "menubar": "file edit view insert format tools table help",
     "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount",
     "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment code",
